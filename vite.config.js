bad779f5fa7d0d6132b55eb516b91fc90c2370ef
Martin Wettig <31127009+Species521@users.noreply.github.com>
2026-06-03 19:08:28 +0200

    capital letter fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/babylon_3dgs/', // This must match your repository name exactly
+  base: '/babylon_3DGS/', // This must match your repository name exactly
 })
